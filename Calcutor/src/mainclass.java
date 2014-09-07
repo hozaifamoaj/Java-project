fd24f0894c692438e2e9179cd44d414e67484a65
hozaifamoaj <hozaifa.moaj@gmail.com>
2014-09-07 22:35:45 +0600

    Calculator

---
+++ Calcutor/src/mainclass.java
@@ -0,0 +1,17 @@
+
+import java.awt.TextField;
+import javax.swing.JFrame;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author isfarahmad
+ */
+public class mainclass extends JFrame
+{
+    private TextField textField1;
+}
